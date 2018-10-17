ac38c1ef04ee250d00c0a29565301f5252a81086
Peter van Hardenberg <pvh@pvh.ca>
2018-10-17 11:52:22 -0700

    add a bunch of comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,42 @@
 
-setTimeout( () => {
-  process.hrtime = require('browser-process-hrtime')
+// Something in the stack is relying on process.hrtime, which browsers lack
+// and the browserify default process object does not include.
+process.hrtime = require('browser-process-hrtime')
 
-// hyperswarm tests go here
 const network = require('@hyperswarm/network')
 const crypto = require('crypto')
+
+// chrome-dgram is not compatible with node dgram in a few ways,
+// usually because chrome-dgram does almost everything asynchronously
+const dgram = require('chrome-dgram')
+
+// we pass a chrome-dgram socket to UTP-wasm which we pass to network()
+// it's the turducken of networking.
 const UTP = require('utp-wasm')
-const dgram = require('dgram')
 
 let socket = dgram.createSocket('udp4')
 socket.bind(20000)
 
-
+// we give the socket.bind() a second to complete :(
 setTimeout(() => {
+  // this won't work for you until you fix the export UTP line (i sent a PR) 
   let utp = UTP({ socket })
+  // there's a PR for this too
   utp.address = () => utp.socket.address()
 
+  // this had better show a port or you'll have more problems soon
   console.log('address', utp.address())
 
-  const net = network({socket: utp})
+  // i had to implement options passing here, i'm sure you can see how
+  const net = network({socket: utp, ephemeral: false})
 
   // look for peers listed under this topic
   const topic = crypto.createHash('sha256')
     .update('my-hyperswarm-topic')
     .digest()
 
+  // this errors with Socket is already bound unless you disable 
+  // the extra listen(0) somehow
   net.join(topic, {
     lookup: true, // find & connect to peers
     announce: true // optional- announce self as a connection target
@@ -34,57 +46,9 @@ setTimeout(() => {
     console.log('new connection!', details)
 
     // you can now use the socket as a stream, eg:
+    // (this doesn't work in a chrome app but since we don't have connections
+    //  i have not yet bothered to put something else in)
     process.stdin.pipe(socket).pipe(process.stdout)
   })
-}, 1000)
 
 }, 1000)
-
-
-/* 
-var socketId;
-
-// Handle the "onReceive" event.
-var onReceive = function(info) {
-  if (info.socketId !== socketId)
-    return;
-  console.log(info.data);
-};
-
-var arrayBuffer = new ArrayBuffer(12)
-var dataView = new DataView(arrayBuffer)
-dataView.setInt32(0, 0x1234ABCD)
-
-// Create the Socket
-chrome.sockets.udp.create({}, function(socketInfo) {
-  socketId = socketInfo.socketId;
-  // Setup event handler and bind socket.
-  chrome.sockets.udp.onReceive.addListener(onReceive);
-  chrome.sockets.udp.bind(socketId,
-    "0.0.0.0", 0, function(result) {
-      if (result < 0) {
-        console.log("Error binding socket.");
-        return;
-      }
-      chrome.sockets.udp.getInfo(socketId, function(socketInfo) {
-        console.log("result:", socketInfo)
-        
-        chrome.sockets.udp.send(socketId, arrayBuffer,
-          '127.0.0.1', socketInfo.localPort, function(sendInfo) {
-            console.log("sent " + sendInfo.bytesSent);
-        });
-      })
-  });
-});
-
-// Create the Socket
-chrome.sockets.udp.create({}, function(socketInfo) {
-  // The socket is created, now we can send some data
-  var socketId = socketInfo.socketId;
-  chrome.sockets.udp.send(socketId, arrayBuffer,
-    '127.0.0.1', 1337, function(sendInfo) {
-      console.log("sent " + sendInfo.bytesSent);
-  });
-});
-
-*/
